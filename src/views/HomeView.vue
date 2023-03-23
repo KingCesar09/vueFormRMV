1d01261946335862c1ab9e719d98c57b0a0b5e3d
KingCesar09 <santiagorf09@gmail.com>
2023-03-23 14:38:13 -0500

    Se inicia el proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="firstName" label="Nombres"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="lastName" label="Apellidos"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="idNumber" label="Número de Identificación"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="expedition" label="Expedido en"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="address" label="Dirección"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="city" label="Ciudad"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="phone" label="Teléfono"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="email" label="Correo Electrónico"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-btn @click="submitForm">Enviar</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      idNumber: '',
+      expedition: '',
+      address: '',
+      city: '',
+      phone: '',
+      email: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      // Logica con baaack-end
+      console.log('Datos del formulario:', this.firstName, this.lastName, this.idNumber, this.expedition, this.address, this.city, this.phone, this.email)
+    },
+  },
 }
 </script>
